f9952ff9b97372cba1d4eebf43e1030c85b57244
yunus oncel <yunus.oncel@gmail.com>
2017-09-24 02:14:06 +0300

    integration with log4j2

--- src/main/java/tr/com/inciborek/controller/Pane.java
+++ src/main/java/tr/com/inciborek/controller/Pane.java
@@ -1,17 +1,18 @@
 package tr.com.inciborek.controller;
 
 import javafx.fxml.Initializable;
-import org.apache.log4j.Logger;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by bk on 23/09/2017.
  */
 public class Pane implements Initializable {
 
-    private static final Logger logger = Logger.getLogger(Pane.class);
+    private static final Logger logger = LogManager.getLogger(Pane.class);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
